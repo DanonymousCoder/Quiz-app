43904bf33e8a6de7f94b7bcea575d34e36b11335
Ridwan <aladebaruwa@gmail.com>
2023-10-28 20:15:01 +0100

    add

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ let form3 = document.getElementById("form3");
 let next1 = document.getElementById("next1");
 let next2 = document.getElementById("next2");
 let back1 = document.getElementById("back1");
-let back2 = document.getElementById("back2");
+
+let progress = document.getElementById("progress");
+
+
 
 next1.onclick = function() {
     form1.style.left = "-450px";
