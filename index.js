d82d1e974b9ef45cf29c8d69cf97e19bb90fe088
Ridwan <aladebaruwa@gmail.com>
2023-10-28 18:16:14 +0100

    add

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 let questions = [
     {
         question: "Which company produces the iPhone?",
-        options : ["Apple", "Microsoft", "Android", "Samsung"]
+        options : ["Apple", "Microsoft", "Android", "Samsung"],
+        answer: "Apple"
+    },
+    {
+        question: "Which company produces the iPhone?",
+        options : ["Apple", "Microsoft", "Android", "Samsung"],
+        answer: "Apple"
+    },
+    {
+        question: "Which company produces the iPhone?",
+        options : ["Apple", "Microsoft", "Android", "Samsung"],
+        answer: "Apple"
     },
 ]
+
+let quizCont = document.querySelector(".quiz-cont")
