8c991a89221947c4767160e716a62ede5ad67f44
Ridwan <aladebaruwa@gmail.com>
2023-10-28 20:10:59 +0100

    add

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ next2.onclick = function() {
 }
 back2.onclick = function() {
     form2.style.left = "40px";
-    form2.style.left = "450px"
+    form3.style.left = "450px"
 }
 
 let questions = [
