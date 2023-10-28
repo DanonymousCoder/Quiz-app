db7f36b8dd9c122a985b7312d7050cafd9e7d635
Ridwan <aladebaruwa@gmail.com>
2023-10-28 19:56:19 +0100

    add

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let form3 = document.getElementById("form3");
 let next1 = document.getElementById("next1");
 let next2 = document.getElementById("next2");
 let back1 = document.getElementById("back1");
+let back2 = document.getElementById("back2");
 
 let questions = [
     {
