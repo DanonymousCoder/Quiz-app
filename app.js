95ab4c1d865ccf1f852da14c5f5b6b64aedc0248
Ridwan <aladebaruwa@gmail.com>
2023-10-29 20:00:10 +0100

    add

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ let h4 = document.getElementById("form2");
 let quizArray = questions.map((quiz) => {
     return `
     <div class="box">
-    <h4 class="h4">${quiz.question}</h4>
+    <p class="h4">${quiz.question}</p>
     <select name="" id="" style="width: 100%; padding: 10px 5px;margin: 5px 0;border: 0;border-bottom: 1px solid #999; outline: none;background: transparent;">
         <option value="" disabled>Select a value</option>
         <option value=""></option>
