ffa6a1a07c870b4055757aca40a50ba376ee7445
Ridwan <aladebaruwa@gmail.com>
2023-10-30 18:03:10 +0100

    add

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function snext2() {
     form3.style.left = "40px";
     progress.style.width = "360px";
 }
-function back() {
+function backtwo() {
     form2.style.left = "40px";
     form3.style.left = "450px";
     progress.style.width = "240px";
