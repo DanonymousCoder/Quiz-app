684b92c3904212372617a130976945e04a798d68
Ridwan <aladebaruwa@gmail.com>
2023-10-30 18:24:05 +0100

    add

--- app.js
+++ app.js
@@ -73,7 +73,25 @@ h4.innerHTML = `${quizArray.join (" ") } <br> <br> ${content}` ;
 let selectS = document.querySelectorAll("select")
 
 selectS.forEach((select, index) => {
-    
+    select.addEventListener(("change", (event) => {
+        let selectValue = event.target.value;
+        let picked = document.querySelectorAll(".selected")[index];
+        picked.innerHTML = `Selected answer: ${selectValue}`;
+        let correct = document.querySelectorAll(".correct")[index];
+        correct.innerHTML = `Correct answer: ${quizData[index].answer}`;
+  
+        if (quizData[index].answer === selectValue) {
+          document.querySelectorAll(".status")[
+            index
+          ].innerHTML = `Status: Correct`;
+          result += corrects;
+          document.querySelector(".result").innerHTML = `${result}/150`;
+        } else {
+          document.querySelectorAll(".status")[index].innerHTML = `Status: Wrong`;
+          result += wrong;
+          document.querySelector(".result").innerHTML = `${result}/150`;
+        }
+    }))
 })
 
 }
