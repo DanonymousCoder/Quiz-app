4abad49d77fe5d907cd1c9e27f58bc4bd4f84039
Ridwan <aladebaruwa@gmail.com>
2023-10-30 19:56:44 +0100

    add

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ let progress = document.getElementById("progress");
 
 let btn = document.getElementById("btn-box")
 let content = btn.innerHTML;
+/*
 let btnn = document.getElementById("btn-boxx")
 let contentt= btn.innerHTML;
 let totScore = document.querySelector(".score")
+*/ 
 
 next1.onclick = function() {
     form1.style.left = "-450px";
