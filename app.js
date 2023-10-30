8fe477c08294cf8fcc1970b02bbb9693a25681d6
Ridwan <aladebaruwa@gmail.com>
2023-10-30 19:20:42 +0100

    add

--- app.js
+++ app.js
@@ -78,12 +78,7 @@ function results() {
 
 let quizResult = questions.map((quiz, index) => {
     return `
-    <div class="box">
-    <h4>${quiz.question}</h4>
-    <p class="selected">Selected answer: </p>
-    <p class="correct">Correct answer: </p>
-    <p class="status">Status: </p>
-  </div>
+    
     `
 let selectS = document.querySelectorAll("select");
 
