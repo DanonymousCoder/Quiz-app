bff43993fc2f13cf7193c0856e82bdd5a17d00bf
Ridwan <aladebaruwa@gmail.com>
2023-10-30 20:09:58 +0100

    add

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ let next2 = document.getElementById("next2");
 let back2 = document.getElementById("back1");
 
 let progress = document.getElementById("progress");
-
+let top = document.querySelector(".top");
+let contenx = top.innerHTML;
 let btn = document.getElementById("btn-box")
 let content = btn.innerHTML;
 let btnn = document.getElementById("btn-boxx")
